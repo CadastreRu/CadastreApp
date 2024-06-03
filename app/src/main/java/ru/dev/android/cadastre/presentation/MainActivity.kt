1df8faaca125b3bb873d4d18e67139d83a3c99d5
CadastreRu <cadastredev@yandex.ru>
2024-06-03 17:19:14 +0300

    Add layers

--- app/src/main/java/ru/dev/android/cadastre/presentation/MainActivity.kt
+++ app/src/main/java/ru/dev/android/cadastre/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package ru.dev.android.cadastre
+package ru.dev.android.cadastre.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import ru.dev.android.cadastre.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
