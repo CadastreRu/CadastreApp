9ddd4bf8a7f5a169c3fc1c384c4a3f8fe9d1e20d
CadastreRu <cadastredev@yandex.ru>
2024-06-04 17:30:07 +0300

    Create domain layer for news

--- app/src/main/java/ru/dev/android/cadastre/domain/news/entity/News.kt
+++ app/src/main/java/ru/dev/android/cadastre/domain/news/entity/News.kt
@@ -3,7 +3,6 @@ package ru.dev.android.cadastre.domain.news.entity
 data class News(
     val id: String,
     val title: String,
-    val type: String,
     val annotation: String,
     val text: String?,
     val date: String,
