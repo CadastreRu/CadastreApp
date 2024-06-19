a5ba4f40b1cf55dcd23afdb0865a0e5c93ed4c91
CadastreRu <cadastredev@yandex.ru>
2024-06-19 17:58:59 +0300

    Adding okHttpClient

--- app/src/main/java/ru/dev/android/cadastre/data/api/ApiFactory.kt
+++ app/src/main/java/ru/dev/android/cadastre/data/api/ApiFactory.kt
@@ -1,19 +1,26 @@
 package ru.dev.android.cadastre.data.api
 
 import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.create
+import ru.dev.android.cadastre.BuildConfig
 
 object ApiFactory {
 
+    private const val HEADER_REQUEST_KEY = "Authorization"
     private const val BASE_URL = "https://api.cadastre.ru/v1/"
 
     private val okHttpClient = OkHttpClient.Builder()
-        .addInterceptor(HttpLoggingInterceptor().apply {
-            level = HttpLoggingInterceptor.Level.BODY
-        }).build()
+        .addInterceptor { chain ->
+            val originalRequest = chain.request()
+            val newRequest = originalRequest
+                .newBuilder()
+                .url(originalRequest.url)
+                .addHeader(HEADER_REQUEST_KEY, BuildConfig.CADASTRE_API_KEY)
+                .build()
+            chain.proceed(newRequest)
+        }.build()
 
     private val retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
